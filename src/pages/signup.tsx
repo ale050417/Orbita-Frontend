8072cf4e39ce42fc441d3b3d2e6b31e67ec6aff0
Mateo Rojas <mateorojasarce2003@gmail.com>
2026-05-18 17:54:08 -0300

    rubro-onboarding

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,3 +1,9 @@
+import type { GetServerSideProps } from 'next';
+
+export const getServerSideProps: GetServerSideProps = async () => ({
+  redirect: { destination: '/onboarding/rubro', permanent: false },
+});
+
 export default function SignupPage() {
   return null;
 }
